9f839f144a52e60044532a6fedc491f5de3fc0dd
uoehoswi3 <thdus1821@naver.com>
2022-06-26 17:58:03 +0900

    fourth commit

---
+++ app/src/main/java/com/example/healthapp/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
